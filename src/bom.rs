68c17bc34ab76f4cedbc446e76cbbcf8ee3f2818
Ted Driggs <ted.driggs@outlook.com>
2021-05-14 08:55:57 -0700

    Add Bom::metadata with timestamp

--- src/bom.rs
+++ src/bom.rs
@@ -25,18 +25,29 @@ pub struct Bom<'a> {
     #[serde(serialize_with = "uuid_to_urn")]
     serial_number: Uuid,
     version: u32,
+    metadata: Metadata<'a>,
     components: Vec<Component<'a>>,
+}
+
+impl<'a> Default for Bom<'a> {
+    fn default() -> Self {
+        Self {
+            bom_format: BomFormat::CycloneDX,
+            spec_version: "1.3",
+            version: 1,
+            serial_number: Uuid::new_v4(),
+            metadata: Metadata::default(),
+            components: vec![],
+        }
+    }
 }
 
 /// Create a new BOM from a sequence of cargo package references.
 impl<'a> FromIterator<&'a Package> for Bom<'a> {
     fn from_iter<T: IntoIterator<Item = &'a Package>>(iter: T) -> Self {
         Self {
-            bom_format: BomFormat::CycloneDX,
-            spec_version: "1.3",
-            version: 1,
-            serial_number: Uuid::new_v4(),
             components: iter.into_iter().map(Component::from).collect(),
+            ..Default::default()
         }
     }
 }
@@ -48,6 +59,8 @@ impl ToXml for Bom<'_> {
         xml.attr("serialNumber", &self.serial_number.to_urn().to_string())?;
         xml.attr("version", "1")?;
         xml.attr("xmlns", "http://cyclonedx.org/schema/bom/1.1")?;
+
+        self.metadata.to_xml(xml)?;
 
         xml.begin_elem("components")?;
         for component in &self.components {
